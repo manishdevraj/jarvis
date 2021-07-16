79a036e15416d4e1ff5d3fd5e6ed82b60e4711cb
Manish Devraj <manish.devraj@gmail.com>
2021-07-16 13:16:14 +0530

    comments

--- src/main/java/org/javainaction/twopointers/RemoveAllAdjDuplicate.java
+++ src/main/java/org/javainaction/twopointers/RemoveAllAdjDuplicate.java
@@ -23,12 +23,26 @@ package org.javainaction.twopointers;
  */
 public class RemoveAllAdjDuplicate {
     public static String removeDuplicates(String s) {
+        //index with distinct adjacent values
         int i = 0;
+        //character array to manipulate in place
         char[] chars = s.toCharArray();
 
         for (int  j = 0; j < chars.length; j++, i++) {
+            //assign ith place with jth character
             chars[i] = chars[j];
+
+            //remember we always increment i even we found duplicate
+            //because i = i - 2 makes sure we have distinct adjacent elements from 0 to i so
+            //i + 1 makes sure we are trying for new location if we have distinct element
+            //abbaca
+            //at j = 2 [a, b, b....] i becomes 0 pointing at a
+            //at j = 3, i becomes 1 and we assign next letter a at i [a, a, ....] and since they are same make i - 2
+            //at j = 4, i becomes 0 and we assign [c,...]
+            //at j = 5, i becomes 1 and we assign [c,a] and we reached end of string
+            //so we return ca
             if (i > 0 && chars[i - 1] == chars[i]) {
+                //go back 2 step if previous character and current character are same to account for adjacent length of 2
                 i = i - 2;
             }
         }
@@ -39,5 +53,7 @@ public class RemoveAllAdjDuplicate {
     public static void main(String[] args){
         System.out.println(removeDuplicates("abbaca"));
         System.out.println(removeDuplicates("azxxzy"));
+        System.out.println(removeDuplicates("acaaabbbacdddd"));
+        System.out.println(removeDuplicates("mississipie"));
     }
 }
